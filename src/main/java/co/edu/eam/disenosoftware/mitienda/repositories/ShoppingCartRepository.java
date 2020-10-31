a59c7ec82fd870ecb3581bc3a56ed0b01e098a49
ErrorHyperion <juan.villanueva.1447@eam.edu.co>
2020-10-31 17:33:04 -0500

    deleteProductToShoppingCart API

--- src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/ShoppingCartRepository.java
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/ShoppingCartRepository.java
@@ -1,8 +1,36 @@
 package co.edu.eam.disenosoftware.mitienda.repositories;
 
+import co.edu.eam.disenosoftware.mitienda.exceptions.APIException;
+import co.edu.eam.disenosoftware.mitienda.model.responses.ErrorResponse;
+import co.edu.eam.disenosoftware.mitienda.util.APIErrorHandler;
+import co.edu.eam.disenosoftware.mitienda.util.RetroFitUtils;
+import co.edu.eam.disenosoftware.mitienda.webservices.ShoppingCartAPIClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import java.io.IOException;
+
 /**
  * Repository to access shoppingcart and shoppingcartproduct data
  */
 public class ShoppingCartRepository {
+
+  public void deleteProductToShoppingCart(Long idShoppingCart, Long idShoppingCartProduct) throws IOException {
+
+    ShoppingCartAPIClient apiClient = RetroFitUtils.buildAPIClient(ShoppingCartAPIClient.class);
+
+    //Hacer el Request
+    Call<Void> request = apiClient.deleteProductToShoppingCart(idShoppingCart, idShoppingCartProduct);
+    Response<Void> response = request.execute();
+
+    //Pocesar la respuesta
+    if (!response.isSuccessful()) {
+      throw APIErrorHandler.throwApiException(response);
+    }
+
+  }
 
 }
--- src/main/java/co/edu/eam/disenosoftware/mitienda/webservices/ShoppingCartAPIClient.java
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/webservices/ShoppingCartAPIClient.java
@@ -1,8 +1,18 @@
 package co.edu.eam.disenosoftware.mitienda.webservices;
 
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.PATCH;
+import retrofit2.http.Path;
+
 /**
  * ShoppingCart and ShoppingCartProduct api web service client
  */
 public interface ShoppingCartAPIClient {
+
+  public static final String SHOPPING_CART_URL = "/api/shopping-cart";
+
+  @DELETE(SHOPPING_CART_URL + "/{idShoppingCart}/shopping-cart-product/{idShoppingCartProduct}")
+  Call<Void> deleteProductToShoppingCart(@Path("idShoppingCart") Long idShoppingCart, @Path("idShoppingCartProduct") Long idShoppingCartProduct);
 
 }
