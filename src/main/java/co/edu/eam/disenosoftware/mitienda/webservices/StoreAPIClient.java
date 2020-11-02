abe292a5699fe9e4f098da7daf836b278a97584d
GilberttValentine <pablo.guzman.4983@eam.edu.co>
2020-11-02 15:54:13 -0500

    Adding API getAllStoresOpen

--- src/main/java/co/edu/eam/disenosoftware/mitienda/webservices/StoreAPIClient.java
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/webservices/StoreAPIClient.java
@@ -1,7 +1,19 @@
 package co.edu.eam.disenosoftware.mitienda.webservices;
 
+import co.edu.eam.disenosoftware.mitienda.model.entities.Store;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+import java.util.List;
+
 /**
  * Store api client
  */
 public interface StoreAPIClient {
+
+  public static final String STORES_URL = "/api/stores/";
+
+  @GET(STORES_URL + "is-open")
+  Call<List<Store>> getAllStoresOpen();
+
 }
