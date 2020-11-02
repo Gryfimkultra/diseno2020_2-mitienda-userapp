619678bddb433137ec98d7f921514ca2c8515802
Filowannabe <felipe8888887@gmail.com>
2020-11-02 17:21:05 -0500

    Felipe Corredor - Adding the geOrderById web service

--- src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/OrdersRepository.java
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/OrdersRepository.java
@@ -1,8 +1,27 @@
 package co.edu.eam.disenosoftware.mitienda.repositories;
 
+import co.edu.eam.disenosoftware.mitienda.model.entities.Order;
+import co.edu.eam.disenosoftware.mitienda.util.APIErrorHandler;
+import co.edu.eam.disenosoftware.mitienda.util.RetroFitUtils;
+import co.edu.eam.disenosoftware.mitienda.webservices.OrderAPIClient;
+import retrofit2.Call;
+import retrofit2.Response;
+
+import java.io.IOException;
+
 /**
  * Repository to access orders data
  */
 public class OrdersRepository {
 
+  public Order getOrderById(long id) throws IOException {
+    OrderAPIClient orderAPIClient = RetroFitUtils.buildAPIClient(OrderAPIClient.class);
+    Call<Order> request = orderAPIClient.getOrderById(id);
+    Response<Order> response = request.execute();
+
+    if (response.isSuccessful()) {
+      return response.body();
+    }
+    throw APIErrorHandler.throwApiException(response);
+  }
 }
