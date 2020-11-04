c591c3d643cb1f8e7eed505f33ab90030480f49a
saralucia1224 <sara.carmona.7927@eam.edu.co>
2020-11-03 22:05:14 -0500

    Arreglos

--- src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/ProductsRepository.java
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/ProductsRepository.java
@@ -1,16 +1,12 @@
 package co.edu.eam.disenosoftware.mitienda.repositories;
 
-import co.edu.eam.disenosoftware.mitienda.exceptions.APIException;
 import co.edu.eam.disenosoftware.mitienda.model.entities.Product;
-import co.edu.eam.disenosoftware.mitienda.model.responses.ErrorResponse;
 import co.edu.eam.disenosoftware.mitienda.util.APIErrorHandler;
 import co.edu.eam.disenosoftware.mitienda.util.RetroFitUtils;
 import co.edu.eam.disenosoftware.mitienda.webservices.ProductAPIClient;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import retrofit2.Call;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
+
 
 import java.io.IOException;
 import java.util.List;
