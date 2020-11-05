6ae16caee41a08ab139261d162208b61a2755cf9
SebastianObando <sebas.obando1724@gmail.com>
2020-11-04 22:07:00 -0500

    rebase 3

--- src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/ShoppingCartRepository.java
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/ShoppingCartRepository.java
@@ -1,15 +1,17 @@
 package co.edu.eam.disenosoftware.mitienda.repositories;
 
 import co.edu.eam.disenosoftware.mitienda.exceptions.APIException;
+import co.edu.eam.disenosoftware.mitienda.model.requests.AddShoppingCartProductRequest;
 import co.edu.eam.disenosoftware.mitienda.model.responses.ErrorResponse;
 import co.edu.eam.disenosoftware.mitienda.util.APIErrorHandler;
 import co.edu.eam.disenosoftware.mitienda.util.RetroFitUtils;
 import co.edu.eam.disenosoftware.mitienda.webservices.ShoppingCartAPIClient;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import retrofit2.Call;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import java.io.IOException;
+
+
 
 import java.io.IOException;
 
@@ -18,10 +20,24 @@ import java.io.IOException;
  */
 public class ShoppingCartRepository {
 
+  public void addProductToShoppingCart(AddShoppingCartProductRequest body) throws IOException {
+    //crear el cliente del api - crear quien hace el requst
+    ShoppingCartAPIClient apiClient = RetroFitUtils.buildAPIClient(ShoppingCartAPIClient.class);
+
+    //hacer equest
+    Call<Void> request = apiClient.addProductToShoppingCart(body);
+    Response<Void> response = request.execute();
+
+    //procesar respuesta
+    if (!(response.isSuccessful())) {
+      throw APIErrorHandler.throwApiException(response);
+    }
+  }
+
   /**
    * delete product from shopping cart
-   * @param idShoppingCart
-   * @param idShoppingCartProduct
+   * @param idShoppingCart idShoppingCart
+   * @param idShoppingCartProduct idShoppingCartProduct
    * @throws IOException
    */
   public void deleteProductToShoppingCart(Long idShoppingCart, Long idShoppingCartProduct) throws IOException {
